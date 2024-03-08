b31d5bd268cecfc78e26b4796f467eaffa586c50
Mado007 <mahmodeid2019@gmail.com>
2024-03-08 22:11:40 +0200

    with grid func fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,77 @@
 #!/usr/bin/python3
-"""Define islandPerimeter function."""
-from typing import List
+""" This is a bad way to get island perimeter """
 
 
-def island_perimeter(grid: List[List[int]]) -> int:
-    """Retrun the perimeter of the island represented by `grid`."""
-    perimeter = 0
-    height = len(grid)
-    width = len(grid[0])  # grid is rectangular, same width for each row
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:  # if land cell
-                perimeter += 4
-                # check the left cell
-                if i > 0 and grid[i - 1][j] == 1:
-                    # then there is two edges common in between
-                    perimeter -= 2
-                # check the above cell
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+def island_perimeter(grid):
+    """ Islands perimeter and i am ashamed"""
+    sum = 0
+    for i in range(len(grid)):
+        for x in range(len(grid[i])):
+            if grid[i][x] == 1 and (i < (len(grid) - 1) and i > 0):
+                fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1)
+                fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
+                fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == (len(grid) - 1)):
+                fb_ver2 = (grid[i - 1][x] != 1 or True)
+                fb_ver_n = (grid[i - 1][x] != 1 and True)
+                fb_ver = (grid[i - 1][x] == 1 and False)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == 0):
+                fb_ver2 = (True or grid[i + 1][x] != 1)
+                fb_ver_n = (True and grid[i + 1][x] != 1)
+                fb_ver = (False and grid[i + 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+    return sum
 
-    return perimeter
+
+def total(grid, i, x, fb_ver, fb_ver2, fb_ver_n):
+    """ get total perimeter now """
+    sum = 0
+    if x == 0 and grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x + 1] != 1:
+            sum += 3
+        elif fb_ver_n and grid[i][x + 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x + 1] == 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x + 1] == 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] == 1:
+            sum += 1
+    elif (x != len(grid[i]) - 1) and grid[i][x] == 1:
+        fb_hor = (grid[i][x + 1] == 1 and grid[i][x - 1] == 1)
+        fb_hor_n = (grid[i][x + 1] != 1 and grid[i][x - 1] != 1)
+        fb_hor2 = (grid[i][x + 1] != 1 or grid[i][x - 1] != 1)
+        if (fb_hor2 and (not fb_hor_n)) and (fb_ver2 and (not fb_ver_n)):
+            sum += 2
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver_n:
+            sum += 3
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver:
+            sum += 1
+        elif fb_hor and fb_ver_n:
+            sum += 2
+        elif fb_hor and (fb_ver2 and (not fb_ver_n)):
+            sum += 1
+        elif fb_hor_n and fb_ver_n:
+            sum += 4
+        elif fb_hor_n and fb_ver:
+            sum += 2
+        elif fb_hor_n and (fb_ver2 and (not fb_ver_n)):
+            sum += 3
+    elif grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x - 1] != 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x - 1] == 1:
+            sum += 2
+        elif fb_ver_n and grid[i][x - 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x - 1] == 1:
+            sum += 3
+        elif fb_ver and grid[i][x - 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x - 1] == 1:
+            sum += 1
+    return sum
